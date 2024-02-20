e5166a2adac855f25c0102f80e5bb2a00260b7ba
Atman Darji <atmandarji172@gmail.com>
2024-02-20 19:49:20 +0530

    refactor: modify gitignore and remove dist file

--- api/dist/utilities/constant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CONSTANTS = void 0;
-exports.CONSTANTS = {
-    JWT_SECRET: 'b0oK_m@n@gEme&t123',
-    PASSWORD_PATTERN: /^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$/,
-    PHONE_PATTERN: /^[6-9]\d{9}$/
-};
-//# sourceMappingURL=constant.js.map
